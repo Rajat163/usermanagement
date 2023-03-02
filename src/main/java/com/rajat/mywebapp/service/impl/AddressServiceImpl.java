dcce921357e08ceefd44210545214d572bb5d885
Rajat163 <rajatsaxena2816@gmail.com>
2023-03-02 19:22:10 +0530

    Implementing get all customers functionality. Written code to add new address

--- src/main/java/com/rajat/mywebapp/service/impl/AddressServiceImpl.java
+++ src/main/java/com/rajat/mywebapp/service/impl/AddressServiceImpl.java
@@ -1,14 +1,42 @@
 package com.rajat.mywebapp.service.impl;
 
 import com.rajat.mywebapp.models.dtos.AddressDTO;
+import com.rajat.mywebapp.models.entities.Address;
+import com.rajat.mywebapp.repositories.AddressRepo;
 import com.rajat.mywebapp.service.interfaces.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddressServiceImpl implements AddressService {
+    @Autowired
+    private AddressRepo repo;
+
     @Override
     public void addNewAddress(AddressDTO dto) {
+        Address address = new Address();
+        if (dto.getAddressLine1() != null) {
+            address.setAddressLine1(dto.getAddressLine1());
+        }
 
+        if (dto.getAddressLine2() != null) {
+            address.setAddressLine2(dto.getAddressLine2());
+        }
+
+        if (dto.getCity() != null) {
+            address.setCity(dto.getCity());
+        }
+
+        if (dto.getCountry() != null) {
+            address.setCountry(dto.getCountry());
+        }
+
+        if (dto.getPincode() > 0) {
+            address.setPincode(dto.getPincode());
+        }
+
+        address.setPrimaryAddress(dto.isPrimaryAddress());
+        repo.save(address);
     }
 
     @Override
--- src/main/java/com/rajat/mywebapp/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/rajat/mywebapp/service/impl/CustomerServiceImpl.java
@@ -4,22 +4,26 @@ import com.rajat.mywebapp.models.dtos.CustomerDTO;
 import com.rajat.mywebapp.models.entities.Customer;
 import com.rajat.mywebapp.repositories.CustomerRepo;
 import com.rajat.mywebapp.service.interfaces.CustomerService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Autowired
     private CustomerRepo repo;
+    @Autowired
+    private ModelMapper mapper;
 
     @Override
     public void addNewCustomer(CustomerDTO dto) {
         Customer customer = new Customer();
 
         if (dto.getFirstName() != null) {
-            customer.setFirstName(dto.getFirstName() );
+            customer.setFirstName(dto.getFirstName());
         }
         if (dto.getLastName() != null) {
             customer.setLastName(dto.getLastName());
@@ -30,9 +34,9 @@ public class CustomerServiceImpl implements CustomerService {
         if (dto.getOrders() > 0) {
             customer.setOrders(dto.getOrders());
         }
-
+        repo.save(customer);
     }
-    
+
 
     @Override
     public void removeExistingCustomer(String custID) {
@@ -46,11 +50,17 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public List<CustomerDTO> getAllCustomers() {
-        return null;
+        List<Customer> customers = repo.findAll();
+        List<CustomerDTO> customerDTOS = new ArrayList<>();
+        customers.forEach(customer -> {
+            CustomerDTO dto = mapper.map(customer, CustomerDTO.class);
+            customerDTOS.add(dto);
+        });
+        return customerDTOS;
     }
 
     @Override
-    public CustomerDTO updateCustomer(String custId,CustomerDTO dto) {
+    public CustomerDTO updateCustomer(String custId, CustomerDTO dto) {
         return null;
     }
 
