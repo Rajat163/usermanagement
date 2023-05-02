e05668e4f822e65555133b559dbf62203c6948b8
Rajat163 <rajatsaxena2816@gmail.com>
2023-05-02 21:50:05 +0530

    Basic Routes for Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Customer from "./components/Customer";
+import CustomerCreate from "./components/CustomerCreate";
+import CustomerFind from "./components/CustomerFind";
 
 function App() {
-  return <Customer />;
+  return (
+    <Routes>
+      <Route path="/" element={<Customer />} />
+      <Route path="/create-customer" element={<CustomerCreate />} />
+      <Route path="/find-customer" element={<CustomerFind />} />
+    </Routes>
+  );
 }
 
 export default App;
